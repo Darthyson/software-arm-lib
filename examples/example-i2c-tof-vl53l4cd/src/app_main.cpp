0d9333afc1e56cc8a9696f85d3855d33ae1da036
Darthyson <darth@maptrack.de>
2024-08-27 14:15:08 +0200

    Add VL53L4CD ToF (Time of Flight) sensor support

---
+++ examples/example-i2c-tof-vl53l4cd/src/app_main.cpp
@@ -0,0 +1,119 @@
+/**************************************************************************//**
+ * @addtogroup SBLIB_EXAMPLES Selfbus library usage examples
+ * @defgroup SBLIB_EXAMPLE_I2C_TOF_VL43L4CD i2c VL53L4CD Time of Flight (ToF) sensor example
+ * @ingroup SBLIB_EXAMPLES
+ * @brief   Configures and reads VL53L4CD range
+ * @note Use
+ * @{
+ ******************************************************************************/
+
+/*
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 3 as
+ published by the Free Software Foundation.
+ ---------------------------------------------------------------------------*/
+
+#include <sblib/types.h>
+#include <sblib/io_pin_names.h>
+#include <sblib/eibBCU1.h>
+#include <sblib/serial.h>
+#include <sblib/i2c/VL53L4CD_api.h>
+#include <sblib/i2c/VL53L4CD_calibration.h>
+
+constexpr uint8_t VL53L4CD_DEFAULT_I2C_ADDRESS = 0x29;
+BCU1 bcu = BCU1();
+
+void failHardInDebug(VL53L4CD_Error status)
+{
+#ifndef DEBUG
+    return;
+#endif
+
+    if (status != VL53L4CD_ERROR_NONE)
+    {
+        fatalError();
+    }
+}
+
+void calibrateOffset()
+{
+    int16_t measuredOffset;
+    failHardInDebug(VL53L4CD_CalibrateOffset(VL53L4CD_DEFAULT_I2C_ADDRESS, 100, &measuredOffset, 255));
+    serial.println("calibrateOffset measuredOffset=", measuredOffset, DEC, 6);
+}
+
+/**
+ * Initialize the application.
+ */
+BcuBase* setup()
+{
+    // LED Initialize
+    pinMode(PIN_INFO, OUTPUT);
+    digitalWrite(PIN_INFO, false);
+
+    serial.setRxPin(PIN_RX);
+    serial.setTxPin(PIN_TX);
+    serial.setRxPin(PIO1_6);
+    serial.setTxPin(PIO1_7);
+
+    serial.begin(115200);
+    serial.println("Selfbus I2C VL53L4CD Time of Flight (ToF) sensor example");
+    failHardInDebug(VL53L4CD_SensorInit(VL53L4CD_DEFAULT_I2C_ADDRESS));
+    failHardInDebug(VL53L4CD_SetRangeTiming(VL53L4CD_DEFAULT_I2C_ADDRESS, 200, 0));
+    failHardInDebug(VL53L4CD_SetSigmaThreshold(VL53L4CD_DEFAULT_I2C_ADDRESS, 2));
+    failHardInDebug(VL53L4CD_ClearInterrupt(VL53L4CD_DEFAULT_I2C_ADDRESS));
+    //calibrateOffset();
+    failHardInDebug(VL53L4CD_StartRanging(VL53L4CD_DEFAULT_I2C_ADDRESS));
+    failHardInDebug(VL53L4CD_StartTemperatureUpdate(VL53L4CD_DEFAULT_I2C_ADDRESS));
+    failHardInDebug(VL53L4CD_StartRanging(VL53L4CD_DEFAULT_I2C_ADDRESS));
+
+    return (&bcu);
+}
+
+void printResult(const VL53L4CD_ResultsData_t *result)
+{
+    serial.print("state: ", result->range_status, DEC, 3);
+    serial.print(" dist (mm): ", result->distance_mm, DEC, 5);
+    serial.print(" sigma (mm): ", result->sigma_mm, DEC, 5);
+    serial.print(" ambient (kcps): ", result->ambient_rate_kcps, DEC, 5);
+    serial.print(" ambient (kcps/SPAD): ", result->ambient_per_spad_kcps, DEC, 5);
+    serial.print(" target sig (kcps): ", result->signal_rate_kcps, DEC, 5);
+    serial.print(" target sig (kcps/SPAD): ", result->signal_per_spad_kcps, DEC, 5);
+    serial.print(" #SPADs enabled: ", result->number_of_spad, DEC, 5);
+    serial.println();
+}
+
+/**
+ * The main processing loop while no KNX-application is loaded.
+ */
+void loop_noapp()
+{
+    uint8_t dataReady = 0;
+    uint8_t status = VL53L4CD_CheckForDataReady(VL53L4CD_DEFAULT_I2C_ADDRESS, &dataReady);
+    if (status != VL53L4CD_ERROR_NONE)
+    {
+        return;
+    }
+
+    if (dataReady == 0)
+    {
+        return;
+    }
+
+    // Read measured distance. RangeStatus = 0 means valid data
+    VL53L4CD_ResultsData_t result;
+    failHardInDebug(VL53L4CD_GetResult(VL53L4CD_DEFAULT_I2C_ADDRESS, &result));
+    failHardInDebug(VL53L4CD_ClearInterrupt(VL53L4CD_DEFAULT_I2C_ADDRESS));
+    printResult(&result);
+    digitalWrite(PIN_INFO, !digitalRead(PIN_INFO));
+    //failHardInDebug(VL53L4CD_StartRanging(VL53L4CD_DEFAULT_I2C_ADDRESS));
+}
+
+/**
+ * The main processing loop.
+ */
+void loop()
+{
+    // will never be called in this example
+}
+/** @}*/
---
+++ sblib/src/i2c/VL53Lx_i2c.cpp
@@ -0,0 +1,150 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3 as
+ *  published by the Free Software Foundation.
+ */
+
+#include <sblib/i2c/VL53Lx_i2c.h>
+#include <sblib/timer.h>
+#include <sblib/i2c.h>
+
+/**
+ * Initializes I2C communication and sets the clock rate.
+ *
+ * @details This function initializes the I2C communication interface and sets the clock rate to 400kHz.
+ *          It must be called before performing any other I2C operations.
+ */
+static void VL53L4CD_Init()
+{
+    i2c_lpcopen_init();
+    Chip_I2C_SetClockRate(I2C0, 400000);
+}
+
+/**
+ * Reads data from the VL53L4CD sensor.
+ *
+ * @details This function reads data from a specified register of the VL53L4CD sensor.
+ *          It initializes the i2c communication, sets up the transfer structure,
+ *          and performs the i2c read operation.
+ *
+ * @param i2cAddress      The i2c address of the sensor.
+ * @param registerAddress The register address from where data needs to be read.
+ * @param value           A pointer to a buffer where the read data will be stored.
+ * @param size            The size of the data to be read.
+ *
+ * @return @ref VL53LX_ERROR_NONE on success, otherwise @ref VL53LX_ERROR_TIMEOUT
+ */
+static uint8_t VL53L4CD_Read(Dev_t i2cAddress, uint16_t registerAddress, void *value, uint8_t size)
+{
+    VL53L4CD_Init();
+    static_assert(sizeof(registerAddress) == 2);
+    uint8_t reg[sizeof(registerAddress)];
+    reg[0] = (registerAddress >> 8) & 0xFF; // high byte
+    reg[1] = registerAddress & 0xFF;        // low byte
+    I2C_XFER_T xfer = {0};
+    xfer.slaveAddr = i2cAddress;
+    xfer.txBuff = reg;
+    xfer.txSz = sizeof(reg)/sizeof(reg[0]);
+    xfer.rxBuff = reinterpret_cast<uint8_t*>(value);
+    xfer.rxSz = size;
+    while (Chip_I2C_MasterTransfer(I2C0, &xfer) == I2C_STATUS_ARBLOST) {}
+
+    if (xfer.txSz != 0)
+    {
+        return VL53LX_ERROR_TIMEOUT;
+    }
+
+    if (xfer.rxSz != 0)
+    {
+        return VL53LX_ERROR_TIMEOUT;
+    }
+
+    for (uint8_t i = 0; i < size / 2; ++i)
+    {
+      uint8_t temp = reinterpret_cast<uint8_t*>(value)[i];
+      reinterpret_cast<uint8_t*>(value)[i] = reinterpret_cast<uint8_t*>(value)[size - i - 1];
+      reinterpret_cast<uint8_t*>(value)[size - i - 1] = temp;
+    }
+
+    return VL53LX_ERROR_NONE;
+}
+
+/**
+ * Writes data to the VL53L4CD sensor.
+ *
+ * @details This function writes data to a specified register of the VL53L4CD sensor.
+ *          It initializes the i2c communication, prepares the transmission buffer with
+ *          the register address and data, and performs the i2c write operation.
+ *
+ * @param i2cAddress      The i2c address of the sensor.
+ * @param registerAddress The register address where data needs to be written.
+ * @param value           A pointer to the data that needs to be written.
+ * @param size            The size of the data to be written.
+ *
+ * @return @ref VL53LX_ERROR_NONE on success, otherwise @ref VL53LX_ERROR_TIMEOUT
+ */
+static uint8_t VL53L4CD_Write(Dev_t i2cAddress, uint16_t registerAddress, void *value, uint8_t size)
+{
+    VL53L4CD_Init();
+    static_assert(sizeof(registerAddress) == 2);
+    uint8_t txBufferSize = sizeof(registerAddress) + size;
+    uint8_t txBuffer[txBufferSize];
+    txBuffer[0] = (registerAddress >> 8) & 0xff;
+    txBuffer[1] = registerAddress & 0xff;
+    uint8_t *pTxBuffer = &txBuffer[sizeof(registerAddress)];
+
+    // Reverse value bytes
+    for (int i = 0; i < size; ++i)
+    {
+        pTxBuffer[i] = ((uint8_t *)value)[size - 1 - i];
+    }
+
+    uint8_t sent = Chip_I2C_MasterSend(I2C0, i2cAddress, txBuffer, txBufferSize);
+
+    if (sent != txBufferSize)
+    {
+        return VL53LX_ERROR_TIMEOUT;
+    }
+    return VL53LX_ERROR_NONE;
+}
+
+uint8_t VL53L4CD_RdByte(Dev_t i2cAddress, uint16_t registerAddress, uint8_t *value)
+{
+    return VL53L4CD_Read(i2cAddress, registerAddress, value, sizeof(uint8_t));
+}
+
+uint8_t VL53L4CD_RdWord(Dev_t i2cAddress, uint16_t registerAddress, uint16_t *value)
+{
+    return VL53L4CD_Read(i2cAddress, registerAddress, value, sizeof(uint16_t));
+}
+
+uint8_t VL53L4CD_RdDWord(Dev_t i2cAddress, uint16_t registerAddress, uint32_t *value)
+{
+    return VL53L4CD_Read(i2cAddress, registerAddress, value, sizeof(uint32_t));
+}
+
+uint8_t VL53L4CD_WrByte(Dev_t i2cAddress, uint16_t registerAddress, uint8_t value)
+{
+    return VL53L4CD_Write(i2cAddress, registerAddress, &value, sizeof(uint8_t));
+}
+
+uint8_t VL53L4CD_WrWord(Dev_t i2cAddress, uint16_t registerAddress, uint16_t value)
+{
+    return VL53L4CD_Write(i2cAddress, registerAddress, &value, sizeof(uint16_t));
+}
+
+uint8_t VL53L4CD_WrDWord(Dev_t i2cAddress, uint16_t registerAddress, uint32_t value)
+{
+    return VL53L4CD_Write(i2cAddress, registerAddress, &value, sizeof(uint32_t));
+}
+
+uint8_t VL53L4CD_WaitMs(Dev_t i2cAddress, uint32_t TimeMs)
+{
+    if (TimeMs > MAX_DELAY_MILLISECONDS)
+    {
+        delay(TimeMs);
+        return VL53LX_ERROR_NONE;
+    }
+    delayMicroseconds(TimeMs * 1000);
+    return VL53LX_ERROR_NONE;
+}
