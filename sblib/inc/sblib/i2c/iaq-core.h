b5441307b0a31b34caaf973ff455a61dab1695a6
Darthyson <darth@maptrack.de>
2025-03-14 08:49:29 +0100

    [sblib] Fix guard macro was commented out

--- sblib/inc/sblib/i2c/iaq-core.h
+++ sblib/inc/sblib/i2c/iaq-core.h
@@ -7,26 +7,24 @@
  *
  *  Adapted to Selfbus Library by Oliver Stefan (2019)
  */
-
-/*
- * IAQ-CORE Funktionen werden aktuell in keinem Projekt benötigt und wurden noch nicht an die neuen I2C Funktionen angepasst.
- * Daher sind alle IAQ-CORE Funktionen vorerst deaktiviert
- *
-
 #ifndef IAQ_CORE_H
 #define IAQ_CORE_H
-
-class IAQcoreClass{
+///\todo IAQ-CORE Funktionen werden aktuell in keinem Projekt benötigt und
+///      wurden noch nicht an die neuen I2C Funktionen angepasst.
+///      Daher sind alle IAQ-CORE Funktionen vorerst deaktiviert
+/*
+class IAQcoreClass
+{
 private:
-    typedef enum {
+    typedef enum
+    {
         IAQcoreAddress = 0xB4,
     } HUM_SENSOR_T;
+
 public:
     bool Init(void);
     bool GetIAQ(uint16_t &CO2eq, uint8_t &status, uint32_t &resistance, uint16_t &Tvoc);
     bool GetIAQco2(uint16_t &CO2eq);
 };
-
-#endif
-
 */
+#endif
