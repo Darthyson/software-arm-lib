Darthyson/software-arm-lib

commit e8534300dffd58ddde4549ae2a17cb5e438c8072

[sblib] Use `UserEeprom` instead of `BCU1` in `AddrTablesBCU1`
Small step to unmangle all these `BcuBase` and its sub-class dependencies.

sblib/src/eib/bcu1.cpp
@@ -10,15 +10,13 @@
 
 #include <sblib/eib/bcu1.h>
 
-BCU1::BCU1() : BCU1(new UserRamBCU1(), new UserEepromBCU1(), new ComObjectsBCU1(this), new AddrTablesBCU1(this))
-{}
+BCU1::BCU1() : BcuDefault(new UserRamBCU1(), new UserEepromBCU1(), new ComObjectsBCU1(this), nullptr)
+{
+    addrTables = new AddrTablesBCU1(this->userEeprom);
+}
 
 BCU1::BCU1(UserRamBCU1* userRam, UserEepromBCU1* userEeprom, ComObjectsBCU1* comObjects, AddrTablesBCU1* addrTables) :
-        BcuDefault(userRam, userEeprom, comObjects, addrTables),
-        userRam(userRam),
-        userEeprom(userEeprom),
-        comObjects(comObjects),
-        addrTables(addrTables)
+        BcuDefault(userRam, userEeprom, comObjects, addrTables)
 {}
 
 inline void BCU1::begin(int manufacturer, int deviceType, int version)

sblib/inc/sblib/eib/addr_tablesBCU1.h
@@ -11,13 +11,12 @@
 #define sblib_addr_tables_BCU1_h
 
 #include <sblib/eib/addr_tables.h>
-
-class BCU1;
+#include <sblib/eib/userEeprom.h>
 
 class AddrTablesBCU1 : public AddrTables
 {
 public:
-	AddrTablesBCU1(BCU1* bcuInstance) : bcu(bcuInstance) {};
+	AddrTablesBCU1(UserEeprom* aUserEeprom) : userEeprom(aUserEeprom) {};
 	~AddrTablesBCU1() = default;
 
 	/**
@@ -56,7 +55,7 @@ public:
 	byte* assocTable() override;
 
 private:
-	BCU1* bcu;
+    UserEeprom* userEeprom;
 };
 
 #endif /*sblib_addr_tables_BCU1_h*/

sblib/inc/sblib/eib/bcu1.h
@@ -46,16 +46,6 @@ public:
     //  BCU 1, mask version 1.2
     const char* getBcuType() const override { return "BCU1"; }
     uint16_t getMaskVersion() const override { return  0x12; }
-
-    UserRamBCU1* userRam;
-
-    UserEepromBCU1* userEeprom;
-
-    ComObjectsBCU1* comObjects;
-
-    AddrTablesBCU1* addrTables;
-
-protected:
 };
 
 #endif /*sblib_bcu1_h*/

sblib/src/eib/addr_tables.cpp
@@ -10,10 +10,6 @@
 
 #include <sblib/eib/addr_tables.h>
 
-#include <sblib/eib/property_types.h>
-#include <sblib/bits.h>
-#include <sblib/eib/userEeprom.h>
-
 uint16_t AddrTables::addrCount()
 {
     byte* ptrAddrTable = addrTable();

sblib/src/eib/addr_tablesBCU1.cpp
@@ -6,7 +6,6 @@
  */
 
 #include <sblib/eib/addr_tablesBCU1.h>
-#include <sblib/eib/bcu1.h>
 
 int AddrTablesBCU1::indexOfAddr(int addr)
 {
@@ -31,10 +30,10 @@ int AddrTablesBCU1::indexOfAddr(int addr)
 
 byte* AddrTablesBCU1::addrTable()
 {
-    return (byte*) &bcu->userEeprom->addrTabSize();
+    return (byte*) &userEeprom->addrTabSize();
 }
 
 byte* AddrTablesBCU1::assocTable()
 {
-    return bcu->userEeprom->userEepromData + bcu->userEeprom->assocTabPtr();
+    return userEeprom->userEepromData + userEeprom->assocTabPtr();
 }

commit 76a3979ce10e293dee165fcf821aedcc5c710b53

Fix uninitialized `Bus::addressTable` in BCU1
Bug introduced in e853430

sblib/inc/sblib/eib/bus.h
@@ -141,6 +141,8 @@ public:
 
     void setOwnAddress(uint16_t newAddress);
 
+    void setAddressTable(AddrTables* newAddressTable);
+
     /**
      * The received telegram.
      * The higher layer process should not change the telegram data in the buffer!

sblib/src/eib/bcu1.cpp
@@ -13,6 +13,7 @@
 BCU1::BCU1() : BcuDefault(new UserRamBCU1(), new UserEepromBCU1(), new ComObjectsBCU1(this), nullptr)
 {
     addrTables = new AddrTablesBCU1(this->userEeprom);
+    this->bus->setAddressTable(addrTables);
 }
 
 BCU1::BCU1(UserRamBCU1* userRam, UserEepromBCU1* userEeprom, ComObjectsBCU1* comObjects, AddrTablesBCU1* addrTables) :

sblib/src/eib/bus.cpp
@@ -1415,3 +1415,8 @@ void Bus::setOwnAddress(const uint16_t newAddress)
 {
     ownAddress = newAddress;
 }
+
+void Bus::setAddressTable(AddrTables* newAddressTable)
+{
+    addressTable = newAddressTable;
+}