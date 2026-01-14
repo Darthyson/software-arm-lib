Darthyson/software-arm-lib

commit ab744bfd9d8ecfcfab5a712c46185e21b5a921ac

Refactor `knx_lpdu.h`
- Moved inline functions to implementation file, so the compiler can optimize
them for us.
- Moved/renamed lpdu functions from `apci.h` and added test for them

firmware_updater/bootloader/src/bootloader.cpp
@@ -128,9 +128,9 @@ BcuBase* setup()
     serial.println("Boot descriptor page        : 0x", bootDescriptorBlockPage(), HEX);
     serial.println("Boot descriptor size        : 0x", BOOT_BLOCK_DESC_SIZE, HEX);
     serial.print("physical address            : ");
-    serial.print(PHY_ADDR_AREA(physicalAddress));
-    serial.print(".", PHY_ADDR_LINE(physicalAddress));
-    serial.println(".", PHY_ADDR_DEVICE(physicalAddress));
+    serial.print(knxAddressToArea(physicalAddress));
+    serial.print(".", knxAddressToLine(physicalAddress));
+    serial.println(".", knxAddressToDevice(physicalAddress));
     serial.println();
 #endif
 

sblib/inc/sblib/eib/knx_lpdu.h
@@ -22,37 +22,29 @@
 #ifndef SBLIB_KNX_LPDU_H_
 #define SBLIB_KNX_LPDU_H_
 
+#include "sblib/types.h"
 #include <cstdint>
-#include <sblib/types.h>
-#include <sblib/bits.h>
-#include <sblib/utils.h>
 
 
-constexpr uint16_t PHY_ADDR_DEFAULT = 0xffff; //!> default physical address 15.15.255
-constexpr uint16_t PHY_ADDR_BROADCAST = 0;    //!> broadcast address 0.0.0
+/**
+ * Default physical address 15.15.255
+ */
+constexpr uint16_t PHY_ADDR_DEFAULT = 0xffff;
 
-#define PHY_ADDR_AREA(address) ((address >> 12) & 0x0f) //!> Return the area number of a given physical KNX address
-#define PHY_ADDR_LINE(address) ((address >> 8) & 0x0f)  //!> Return the line number of a given physical KNX address
-#define PHY_ADDR_DEVICE(address) (address & 0xff)       //!> Return device number of a given physical KNX address
-
-#define LPDU_CONTROL_BYTE           (0)
-#define LPDU_SENDER_HIGH_BYTE       (1)
-#define LPDU_SENDER_LOW_BYTE        (2)
-#define LPDU_DESTINATION_HIGH_BYTE  (3)
-#define LPDU_DESTINATION_LOW_BYTE   (4)
-
-#define MASK_REPEATED               (1 << 5)
-#define MASK_FRAMETYPE              (1 << 7)
+/**
+ * Broadcast address 0.0.0
+ */
+constexpr uint16_t PHY_ADDR_BROADCAST = 0;
 
 /**
  *  KNX Priority
  */
 enum KNXPriority : uint8_t
 {
-    PRIORITY_SYSTEM = 0, /**< PRIORITY_SYSTEM */
-    PRIORITY_HIGH = 1,   /**< PRIORITY_HIGH */
-    PRIORITY_ALARM = 2,  /**< PRIORITY_ALARM */
-    PRIORITY_LOW = 3     /**< PRIORITY_LOW */
+    PRIORITY_SYSTEM = 0,
+    PRIORITY_HIGH = 1,
+    PRIORITY_ALARM = 2,
+    PRIORITY_LOW = 3
 };
 
 /**
@@ -60,115 +52,116 @@ enum KNXPriority : uint8_t
  */
 enum KNXFrameType
 {
-    FRAME_STANDARD, /**< FRAME_STANDARD */
-    FRAME_EXTENDED  /**< FRAME_EXTENDED */
+    FRAME_STANDARD,
+    FRAME_EXTENDED
 };
 
-void initLpdu(unsigned char* telegram, KNXPriority newPriority, bool newRepeated, KNXFrameType newFrameType);
-byte controlByte(unsigned char* telegram);
-bool isRepeated(unsigned char* telegram);
-void setRepeated(unsigned char* telegram, bool repeated);
-KNXPriority priority(unsigned char* telegram);
-void setPriority(unsigned char* telegram, KNXPriority newPriority);
-uint16_t senderAddress(unsigned char* telegram);
-void setSenderAddress(unsigned char* telegram, uint16_t newSenderAddress);
-uint16_t destinationAddress(unsigned char* telegram);
-void setDestinationAddress(unsigned char* telegram, uint16_t newDestinationAddress);
-KNXFrameType frameType(unsigned char* telegram);
-void setFrameType(unsigned char* telegram, KNXFrameType newFrameType);
+/**
+ * Initialize a KNX LPDU telegram
+ * @param telegram     Pointer to the telegram buffer
+ * @param newPriority  Priority level for the telegram
+ * @param newRepeated  Flag indicating if this is a repeated telegram
+ * @param newFrameType Frame type (standard or extended)
+ */
+void initLpdu(uint8_t* telegram, KNXPriority newPriority, bool newRepeated, KNXFrameType newFrameType);
 
+/**
+ * Get the control byte from a KNX telegram
+ * @param telegram Pointer to the telegram buffer
+ * @return The control byte value
+ */
+byte controlByte(const uint8_t* telegram);
 
-inline void initLpdu(unsigned char* telegram, const KNXPriority newPriority, const bool newRepeated, const KNXFrameType newFrameType)
-{
-    telegram[LPDU_CONTROL_BYTE] = 0xB0;
-    setRepeated(telegram, newRepeated);
-    setPriority(telegram, newPriority);
-    setFrameType(telegram, newFrameType);
-}
+/**
+ * Check if a telegram is repeated
+ * @param telegram Pointer to the telegram buffer
+ * @return true if the telegram is repeated, false otherwise
+ */
+bool isRepeated(const uint8_t* telegram);
 
-inline byte controlByte(unsigned char* telegram)
-{
-    return (telegram[LPDU_CONTROL_BYTE]);
-}
+/**
+ * Set the repeated flag in a telegram
+ * @param telegram Pointer to the telegram buffer
+ * @param repeated true to mark as repeated, false otherwise
+ */
+void setRepeated(uint8_t* telegram, bool repeated);
 
-inline bool isRepeated(unsigned char* telegram)
-{
-    // 5.bit not set => repeated
-    return ((controlByte(telegram) & MASK_REPEATED) == 0);
-}
+/**
+ * Get the priority of a telegram
+ * @param telegram Pointer to the telegram buffer
+ * @return The priority level of the telegram
+ */
+KNXPriority priority(const uint8_t* telegram);
 
-inline void setRepeated(unsigned char* telegram, const bool repeated)
-{
-    if (repeated)
-    {
-        // 5.bit not set => repeated
-        telegram[LPDU_CONTROL_BYTE] &= (uint8_t)~MASK_REPEATED;
-    }
-    else
-    {
-        telegram[LPDU_CONTROL_BYTE] |= MASK_REPEATED;
-    }
-}
+/**
+ * Set the priority of a telegram
+ * @param telegram Pointer to the telegram buffer
+ * @param newPriority The new priority level to set
+ */
+void setPriority(uint8_t* telegram, KNXPriority newPriority);
 
-inline KNXPriority priority(unsigned char* telegram)
-{
-    // 3. and 4. bit control priority
-    return ((KNXPriority)((controlByte(telegram) >> 2) & 0x03));
-}
+/**
+ * Get the sender address from a telegram
+ * @param telegram Pointer to the telegram buffer
+ * @return The sender's physical address
+ */
+uint16_t senderAddress(const uint8_t* telegram);
 
-inline void setPriority(unsigned char* telegram, const KNXPriority newPriority)
-{
-    telegram[LPDU_CONTROL_BYTE] = (uint8_t)(newPriority << 2) | (telegram[LPDU_CONTROL_BYTE] & 0xF3);
-}
+/**
+ * Set the sender address in a telegram
+ * @param telegram Pointer to the telegram buffer
+ * @param newSenderAddress The sender's physical address to set
+ */
+void setSenderAddress(uint8_t* telegram, uint16_t newSenderAddress);
 
-inline uint16_t senderAddress(unsigned char* telegram)
-{
-    return (unsigned short)((telegram[LPDU_SENDER_HIGH_BYTE] << 8) | telegram[LPDU_SENDER_LOW_BYTE]);
-}
+/**
+ * Get the destination address from a telegram
+ * @param telegram Pointer to the telegram buffer
+ * @return The destination address (physical or group)
+ */
+uint16_t destinationAddress(const uint8_t* telegram);
 
-inline void setSenderAddress(unsigned char* telegram, const uint16_t newSenderAddress)
-{
-    telegram[LPDU_SENDER_HIGH_BYTE] = HIGH_BYTE(newSenderAddress);
-    telegram[LPDU_SENDER_LOW_BYTE] = lowByte(newSenderAddress);
-}
+/**
+ * Set the destination address in a telegram
+ * @param telegram Pointer to the telegram buffer
+ * @param newDestinationAddress The destination address to set
+ */
+void setDestinationAddress(uint8_t* telegram, uint16_t newDestinationAddress);
 
+/**
+ * Get the frame type from a telegram
+ * @param telegram Pointer to the telegram buffer
+ * @return The frame type (standard or extended)
+ */
+KNXFrameType frameType(const uint8_t* telegram);
 
-inline uint16_t destinationAddress(unsigned char* telegram)
-{
-    return (makeWord(telegram[LPDU_DESTINATION_HIGH_BYTE], telegram[LPDU_DESTINATION_LOW_BYTE]));
-}
+/**
+ * Set the frame type in a telegram
+ * @param telegram Pointer to the telegram buffer
+ * @param newFrameType The frame type to set (standard or extended)
+ */
+void setFrameType(uint8_t* telegram, KNXFrameType newFrameType);
 
-inline void setDestinationAddress(unsigned char* telegram, const uint16_t newDestinationAddress)
-{
-    telegram[LPDU_DESTINATION_HIGH_BYTE] = HIGH_BYTE(newDestinationAddress);
-    telegram[LPDU_DESTINATION_LOW_BYTE] = lowByte(newDestinationAddress);
-}
+/**
+ * Return the area number of a given physical KNX address
+ * @param address Individual KNX address
+ * @return The area number of the individual address
+ */
+uint8_t knxAddressToArea(uint16_t address);
 
-inline KNXFrameType frameType(unsigned char* telegram)
-{
-    if (controlByte(telegram) & MASK_FRAMETYPE)
-    {
-        return FRAME_STANDARD;
-    }
-    else
-    {
-        return FRAME_EXTENDED;
-    }
-}
+/**
+ * Return the line number of a given physical KNX address
+ * @param address Individual KNX address
+ * @return The line number of the individual address
+ */
+uint8_t knxAddressToLine(uint16_t address);
 
-inline void setFrameType(unsigned char* telegram, const KNXFrameType newFrameType)
-{
-    if (newFrameType == FRAME_EXTENDED)
-    {
-        // 7.bit not set => extended
-        telegram[LPDU_CONTROL_BYTE] &= (uint8_t)~MASK_FRAMETYPE;
-    }
-    else
-    {
-        telegram[LPDU_CONTROL_BYTE] |= MASK_FRAMETYPE;
-    }
-}
-
+/**
+ * Return the device number of a given physical KNX address
+ * @param address Individual KNX address
+ * @return The device number of the individual address
+ */
+uint8_t knxAddressToDevice(uint16_t address);
 
 #endif /* SBLIB_KNX_LPDU_H_ */
 /** @}*/

sblib/src/eib/knx_lpdu.cpp
@@ -1,0 +1,123 @@
+/*
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 3 as
+ published by the Free Software Foundation.
+ ---------------------------------------------------------------------------*/
+
+#include "sblib/eib/knx_lpdu.h"
+#include "sblib/bits.h"
+#include "sblib/utils.h"
+
+
+constexpr uint8_t LPDU_CONTROL_BYTE = 0;
+constexpr uint8_t LPDU_SENDER_HIGH_BYTE = 1;
+constexpr uint8_t LPDU_SENDER_LOW_BYTE = 2;
+constexpr uint8_t LPDU_DESTINATION_HIGH_BYTE = 3;
+constexpr uint8_t LPDU_DESTINATION_LOW_BYTE = 4;
+
+constexpr uint8_t MASK_REPEATED = 1 << 5;
+constexpr uint8_t MASK_FRAMETYPE = 1 << 7;
+
+void initLpdu(uint8_t* telegram, const KNXPriority newPriority, const bool newRepeated, const KNXFrameType newFrameType)
+{
+    telegram[LPDU_CONTROL_BYTE] = 0xB0;
+    setRepeated(telegram, newRepeated);
+    setPriority(telegram, newPriority);
+    setFrameType(telegram, newFrameType);
+}
+
+byte controlByte(const uint8_t* telegram)
+{
+    return telegram[LPDU_CONTROL_BYTE];
+}
+
+bool isRepeated(const uint8_t* telegram)
+{
+    // 5.bit not set => repeated
+    return (controlByte(telegram) & MASK_REPEATED) == 0;
+}
+
+void setRepeated(uint8_t* telegram, const bool repeated)
+{
+    if (repeated)
+    {
+        // 5.bit not set => repeated
+        telegram[LPDU_CONTROL_BYTE] &= static_cast<uint8_t>(~MASK_REPEATED);
+    }
+    else
+    {
+        telegram[LPDU_CONTROL_BYTE] |= MASK_REPEATED;
+    }
+}
+
+KNXPriority priority(const uint8_t* telegram)
+{
+    // 3. and 4. bit control priority
+    return static_cast<KNXPriority>(controlByte(telegram) >> 2 & 0x03);
+}
+
+void setPriority(uint8_t* telegram, const KNXPriority newPriority)
+{
+ // ReSharper disable once CppRedundantParentheses
+    telegram[LPDU_CONTROL_BYTE] = static_cast<uint8_t>(newPriority << 2) | (telegram[LPDU_CONTROL_BYTE] & 0xF3);
+}
+
+uint16_t senderAddress(const uint8_t* telegram)
+{
+    return static_cast<uint16_t>(telegram[LPDU_SENDER_HIGH_BYTE] << 8 | telegram[LPDU_SENDER_LOW_BYTE]);
+}
+
+void setSenderAddress(uint8_t* telegram, const uint16_t newSenderAddress)
+{
+    telegram[LPDU_SENDER_HIGH_BYTE] = HIGH_BYTE(newSenderAddress);
+    telegram[LPDU_SENDER_LOW_BYTE] = lowByte(newSenderAddress);
+}
+
+uint16_t destinationAddress(const uint8_t* telegram)
+{
+    return makeWord(telegram[LPDU_DESTINATION_HIGH_BYTE], telegram[LPDU_DESTINATION_LOW_BYTE]);
+}
+
+void setDestinationAddress(uint8_t* telegram, const uint16_t newDestinationAddress)
+{
+    telegram[LPDU_DESTINATION_HIGH_BYTE] = HIGH_BYTE(newDestinationAddress);
+    telegram[LPDU_DESTINATION_LOW_BYTE] = lowByte(newDestinationAddress);
+}
+
+KNXFrameType frameType(const uint8_t* telegram)
+{
+    if (controlByte(telegram) & MASK_FRAMETYPE)
+    {
+        return FRAME_STANDARD;
+    }
+
+    return FRAME_EXTENDED;
+}
+
+void setFrameType(uint8_t* telegram, const KNXFrameType newFrameType)
+{
+    if (newFrameType == FRAME_EXTENDED)
+    {
+        // 7.bit not set => extended
+        telegram[LPDU_CONTROL_BYTE] &= static_cast<uint8_t>(~MASK_FRAMETYPE);
+    }
+    else
+    {
+        telegram[LPDU_CONTROL_BYTE] |= MASK_FRAMETYPE;
+    }
+}
+
+uint8_t knxAddressToArea(const uint16_t address)
+{
+    return address >> 12 & 0x0f;
+}
+
+uint8_t knxAddressToLine(const uint16_t address)
+{
+    return address >> 8 & 0x0f;
+}
+
+uint8_t knxAddressToDevice(const uint16_t address)
+{
+    return address & 0xff;
+}

sblib/inc/sblib/eib/apci.h
@@ -154,8 +154,5 @@ bool checkApciForMagicWord(byte eraseCode, byte channelNumber);
 ApciCommand apciCommand(unsigned char* telegram);
 void setApciCommand(unsigned char* telegram, ApciCommand newApciCommand, byte additionalData);
 
-uint8_t mainGroup(uint16_t address);
-uint8_t middleGroup(uint16_t address);
-uint8_t lowGroup(uint16_t address);
 
 #endif /* SBLIB_KNX_APCI_H_ */

sblib/inc/sblib/eib/callback_bcu.h
@@ -7,9 +7,9 @@
 #ifndef SBLIB_KNX_CALLBACK_BCU_H_
 #define SBLIB_KNX_CALLBACK_BCU_H_
 
-#include <cstdint>
-#include <sblib/eib/callback_bus.h>
-#include <sblib/eib/bcu_base.h>
+#include "sblib/eib/callback_bus.h"
+#include "sblib/eib/bcu_base.h"
+
 
 class BcuBase;
 

sblib/sblib.cmake
@@ -140,6 +140,7 @@ set(SBLIB_KNX_SRC
         src/eib/com_objectsSYSTEMB.cpp
         src/eib/com_objects_debug.cpp
         src/eib/datapoint_types.cpp
+        src/eib/knx_lpdu.cpp
         src/eib/hardware_descriptor.cpp
         src/eib/knx_tlayer4.cpp
         src/eib/mask0701.cpp

sblib/src/eib/apci.cpp
@@ -51,19 +51,4 @@ bool checkApciForMagicWord(const byte eraseCode, const byte channelNumber)
     return ((eraseCode == BOOTLOADER_MAGIC_ERASE) && (channelNumber == BOOTLOADER_MAGIC_CHANNEL));
 }
 
-uint8_t mainGroup(const uint16_t address)
-{
-    return (highByte(address) >> 3);
-}
-
-uint8_t middleGroup(const uint16_t address)
-{
-    return (highByte(address) & 0x07);
-}
-
-uint8_t lowGroup(const uint16_t address)
-{
-    return (lowByte(address));
-}
-
 /** @}*/

sblib/src/eib/bcu2.cpp
@@ -8,10 +8,12 @@
  *  published by the Free Software Foundation.
  */
 
+#include "sblib/eib/bcu2.h"
+#include "sblib/eib/knx_lpdu.h"
+#include "sblib/internal/iap.h"
+#include "sblib/bits.h"
 #include <cstring>
-#include <sblib/eib/bcu2.h>
-#include <sblib/eib/knx_lpdu.h>
-#include <sblib/internal/iap.h>
+
 
 void BCU2::setOwnAddress(const uint16_t addr)
 {

commit f673ef1f72909592c7e0a73fabfda48338ddaeb0

Fix physical address and group address mix up
Bug introduced in ab744bf

firmware_updater/bootloader/src/bcu_updater.cpp
@@ -140,9 +140,9 @@ bool BcuUpdate::processBroadCastTelegram(ApciCommand apciCmd, unsigned char *tel
     if (handled)
     {
         dump(
-            serial.print(" ", knxAddressToArea(ownAddress()));
-            serial.print(".", knxAddressToLine(ownAddress()));
-            serial.println(".", knxAddressToDevice(ownAddress()));
+            serial.print(" ", physAddressToArea(ownAddress()));
+            serial.print(".", physAddressToLine(ownAddress()));
+            serial.println(".", physAddressToDevice(ownAddress()));
             serial.flush();
         )
 

firmware_updater/bootloader/src/bootloader.cpp
@@ -128,9 +128,9 @@ BcuBase* setup()
     serial.println("Boot descriptor page        : 0x", bootDescriptorBlockPage(), HEX);
     serial.println("Boot descriptor size        : 0x", BOOT_BLOCK_DESC_SIZE, HEX);
     serial.print("physical address            : ");
-    serial.print(knxAddressToArea(physicalAddress));
-    serial.print(".", knxAddressToLine(physicalAddress));
-    serial.println(".", knxAddressToDevice(physicalAddress));
+    serial.print(physAddressToArea(physicalAddress));
+    serial.print(".", physAddressToLine(physicalAddress));
+    serial.println(".", physAddressToDevice(physicalAddress));
     serial.println();
 #endif
 

sblib/inc/sblib/eib/knx_lpdu.h
@@ -147,21 +147,42 @@ void setFrameType(uint8_t* telegram, KNXFrameType newFrameType);
  * @param address Individual KNX address
  * @return The area number of the individual address
  */
-uint8_t knxAddressToArea(uint16_t address);
+uint8_t physAddressToArea(uint16_t address);
 
 /**
  * Return the line number of a given physical KNX address
  * @param address Individual KNX address
  * @return The line number of the individual address
  */
-uint8_t knxAddressToLine(uint16_t address);
+uint8_t physAddressToLine(uint16_t address);
 
 /**
  * Return the device number of a given physical KNX address
  * @param address Individual KNX address
  * @return The device number of the individual address
  */
-uint8_t knxAddressToDevice(uint16_t address);
+uint8_t physAddressToDevice(uint16_t address);
+
+/**
+ * Return the main group of a given KNX group address
+ * @param address Group KNX address
+ * @return The main group of the group address
+ */ 
+uint8_t mainGroupAddress(const uint16_t address);
+
+/**
+ * Return the middle group of a given KNX group address
+ * @param address Group KNX address
+ * @return The middle group of the group address
+ */
+uint8_t middleGroupAddress(const uint16_t address);
+
+/**
+ * Return the low group of a given KNX group address
+ * @param address Group KNX address
+ * @return The low group of the group address
+ */
+uint8_t lowGroupAddress(const uint16_t address);
 
 #endif /* SBLIB_KNX_LPDU_H_ */
 /** @}*/

sblib/src/eib/bus.cpp
@@ -101,9 +101,9 @@ void Bus::begin(const uint16_t physicalAddress)
         serial.println(" ttimer value: ", ttimer.value(), DEC, 6);
         serial.print("nak retries: ", sendRetriesMax, DEC, 2);
         serial.print(" busy retries: ", sendBusyRetriesMax, DEC, 2);
-        serial.print(" phy addr: ", PHY_ADDR_AREA(ownAddress), DEC);
-        serial.print(".", PHY_ADDR_LINE(ownAddress), DEC);
-        serial.print(".", PHY_ADDR_DEVICE(ownAddress), DEC);
+        serial.print(" phy addr: ", physAddressToArea(ownAddress), DEC);
+        serial.print(".", physAddressToLine(ownAddress), DEC);
+        serial.print(".", physAddressToDevice(ownAddress), DEC);
         serial.print(" (0x", ownAddress, HEX, 4);
         serial.println(")");
     ); // DB_BUS

sblib/src/eib/bus_debug.cpp
@@ -19,9 +19,10 @@
  published by the Free Software Foundation.
  ---------------------------------------------------------------------------*/
 
-#include <sblib/eib/bus_debug.h>
-#include <sblib/eib/bus_const.h>
-#include <sblib/eib/knx_lpdu.h>
+#include "sblib/eib/bus_debug.h"
+#include "sblib/eib/bus_const.h"
+#include "sblib/eib/knx_lpdu.h"
+#include "sblib/bits.h"
 
 #if defined(DEBUG_BUS) || defined(DEBUG_BUS_BITLEVEL) || defined (DUMP_TELEGRAMS)
     Timer& ttimer = timer32_0;
@@ -29,9 +30,9 @@
 
 #define dumpKNXAddress(addr) \
          {\
-             serial.print(PHY_ADDR_AREA(addr)); \
-             serial.print(".", PHY_ADDR_LINE(addr)); \
-             serial.print(".", PHY_ADDR_DEVICE(addr)); \
+             serial.print(physAddressToArea(addr)); \
+             serial.print(".", physAddressToLine(addr)); \
+             serial.print(".", physAddressToDevice(addr)); \
          }
 
 #ifdef DUMP_TELEGRAMS

sblib/src/eib/com_objectsBCU1.cpp
@@ -5,13 +5,14 @@
  *      Author: dridders
  */
 
-#include <sblib/eib/com_objectsBCU1.h>
-#include <sblib/eib/bcu1.h>
-#include <sblib/eib/apci.h>
-#include <sblib/utils.h>
+#include "sblib/eib/com_objectsBCU1.h"
+#include "sblib/eib/bcu1.h"
+#include "sblib/eib/apci.h"
+#include "sblib/eib/knx_lpdu.h"
+#include "sblib/utils.h"
 
 #if defined(INCLUDE_SERIAL)
-#   include <sblib/serial.h>
+#   include "sblib/serial.h"
 #endif
 
 int ComObjectsBCU1::objectSize(const int objno)
@@ -74,9 +75,9 @@ void ComObjectsBCU1::processGroupTelegram(const uint16_t addr, const int apci, b
     const int endAssoc = 1 + (*assocTab) * 2;
 
     DB_COM_OBJ(
-        serial.print("grpAddr ", mainGroup(addr));
-        serial.print("/", middleGroup(addr));
-        serial.print("/", lowGroup(addr));
+        serial.print("grpAddr ", mainGroupAddress(addr));
+        serial.print("/", middleGroupAddress(addr));
+        serial.print("/", lowGroupAddress(addr));
         serial.print(": gapos ");
     );
     // Convert the group address into the index of the group address table

sblib/src/eib/knx_lpdu.cpp
@@ -107,17 +107,32 @@ void setFrameType(uint8_t* telegram, const KNXFrameType newFrameType)
     }
 }
 
-uint8_t knxAddressToArea(const uint16_t address)
+uint8_t physAddressToArea(const uint16_t address)
 {
     return address >> 12 & 0x0f;
 }
 
-uint8_t knxAddressToLine(const uint16_t address)
+uint8_t physAddressToLine(const uint16_t address)
 {
     return address >> 8 & 0x0f;
 }
 
-uint8_t knxAddressToDevice(const uint16_t address)
+uint8_t physAddressToDevice(const uint16_t address)
 {
     return address & 0xff;
+}
+
+uint8_t mainGroupAddress(const uint16_t address)
+{
+    return highByte(address) >> 3;
+}
+
+uint8_t middleGroupAddress(const uint16_t address)
+{
+    return highByte(address) & 0x07;
+}
+
+uint8_t lowGroupAddress(const uint16_t address)
+{
+    return lowByte(address);
 }