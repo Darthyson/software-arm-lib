7644b5c7c091262d51c19e60ce96638f5135981b
Darthyson <darth@maptrack.de>
2026-04-17 18:33:19 +0200

    Add macro `FORCE_INLINE` which should work with more compilers
    Credits: https://meghprkh.github.io/blog/posts/c++-force-inline/

--- sblib/inc/sblib/always_inline.h
+++ sblib/inc/sblib/always_inline.h
@@ -20,4 +20,21 @@
 #define ALWAYS_INLINE inline
 #endif
 
+/**
+ * @def FORCE_INLINE
+ * @brief Force a function to be inlined, even if the compiler would not do so by default.
+ * This is more aggressive than ALWAYS_INLINE and may lead to larger code size.
+ * source: https://meghprkh.github.io/blog/posts/c++-force-inline/
+ */
+#if defined(__clang__)
+#   define FORCE_INLINE [[gnu::always_inline]] [[gnu::gnu_inline]] extern inline
+#elif defined(__GNUC__)
+#   define FORCE_INLINE [[gnu::always_inline]] inline
+#elif defined(_MSC_VER)
+#pragma warning(error: 4714)
+#   define FORCE_INLINE __forceinline
+#else
+#   error Unsupported compiler
+#endif
+
 #endif /* SBLIB_ALWAYS_INLINE_H */
