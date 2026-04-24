Darthyson/software-arm-lib

commit 08ff753584675e4a6af9a9ae63d309b4cd3957ce

Prepare for line coupler support, using PA from BootloaderDescriptor

sblib/inc/sblib/internal/bootloader_commands.h
@@ -7,37 +7,67 @@
 #ifndef SBLIB_INTERNAL_BOOTLOADER_COMMANDS_H_
 #define SBLIB_INTERNAL_BOOTLOADER_COMMANDS_H_
 
-#include "sblib/eib/apci.h"
+
 #include <cstdint>
 
+enum class BootState : uint8_t
+{
+    Reset = 0,
+    BootLoader = 1,
+    BootLoaderUpdater = 2,
+    Application = 3,
+};
+
 /**
- * Magic word, which will be checked on startup of the bootloader
- * weather or not to go into bootloader mode
+ * Bootloader descriptor structure stored in RAM to pass parameters to the bootloader.
  */
-constexpr uint32_t BOOTLOADER_MAGIC_WORD = 0x5E1FB055;
+struct BootloaderDescriptor
+{
+    BootState bootState;            //!< The current boot state
+    uint8_t reserved;               //!< Reserved for alignment, feel free to use
+    uint16_t physicalAddress;       //!< Physical address to use in bootloader
+    uint32_t programmingButton;     //!< GPIO of the programming button to use in bootloader
+    uint32_t applicationId;         //!< Application ID of the application
+    uint32_t applicationVersion;    //!< Application version of the application
+};
 
 /**
- * Magic address for the magic word to be checked on startup of the bootloader
- * weather or not to go into bootloader mode
+ * Initialize the BootloaderDescriptor in RAM.
+ *  
+ * @param newBootState          The @ref BootState to set
+ * @param physicalAddressToUse  Physical address to use in bootloader
+ * @param programmingButton     The GPIO of the programming button
+ * @param applicationId         The application ID of the application
+ * @param applicationVersion    The application version of the application
+ * @warning The BootloaderDescriptor is stored in RAM at 0x10000000. 
+ *          The application RAM must start at 0x10000100 or higher to avoid overwriting this structure.
  */
-#define BOOTLOADER_MAGIC_ADDRESS ((uint32_t *) 0x10000000)
-//uint32_t* BOOTLOADER_MAGIC_ADDRESS = reinterpret_cast<uint32_t*>(0x10000000UL);
+void initBootloaderDescriptor(BootState newBootState, uint16_t physicalAddressToUse, uint32_t programmingButton,
+        uint32_t applicationId, uint32_t applicationVersion);
 
 /**
- * Bootloader magic erase = FactoryResetWithoutIndividualAddress in calimero-core
+ * Retrieves the BootloaderDescriptor from RAM.
+ * 
+ * @return Pointer to the BootloaderDescriptor if valid, nullptr otherwise
+ * @warning The BootloaderDescriptor is stored in RAM at 0x10000000. 
+ *          The application RAM must start at 0x10000100 or higher to avoid overwriting this structure.
  */
-constexpr RestartPDUMasterReset BOOTLOADER_MAGIC_ERASE = T_MASTERRESET_FACTORY_WO_IA;
+const BootloaderDescriptor* getBootloaderDescriptor();
 
 /**
- * Bootloader magic channel
+ * Clears the BootloaderDescriptor in RAM.
+ * @warning The BootloaderDescriptor is stored in RAM at 0x10000000. 
+ *          The application RAM must start at 0x10000100 or higher to avoid overwriting this structure.
  */
-constexpr uint8_t BOOTLOADER_MAGIC_CHANNEL = 255;
-
+void clearBootloaderDescriptor();
 
 /**
- * Set magicWord to start in bootloader mode after reset.
- **/
-void prepareRestartIntoBootloader(uint16_t physicalAddressToUse);
+ * Debug-only function to retrieve the BootloaderDescriptor from RAM.
+ * 
+ * @return Pointer to the BootloaderDescriptor in RAM
+ * @warning Use of this function is intended for debugging purposes only.
+ */
+const BootloaderDescriptor* debugOnlyBootloaderDescriptor();
 
 /**
  * Checks a APCI for the bus-updater Magic word
@@ -50,6 +80,6 @@ void prepareRestartIntoBootloader(uint16_t physicalAddressToUse);
  * @return True if apci is a APCI_RESTART_TYPE1_PDU with a magic word<br/>
  *         otherwise false
  */
-bool checkApciForMagicWord(byte eraseCode, byte channelNumber);
+bool checkApciForMagicWord(uint8_t eraseCode, uint8_t channelNumber);
 
 #endif /* SBLIB_INTERNAL_BOOTLOADER_COMMANDS_H_ */

sblib/src/internal/bootloader_commands.cpp
@@ -5,18 +5,88 @@
  ---------------------------------------------------------------------------*/
 
 #include "sblib/internal/bootloader_commands.h"
+#include "sblib/eib/apci.h"
+#include "sblib/platform.h"
 
-void prepareRestartIntoBootloader([[maybe_unused]] const uint16_t physicalAddressToUse)
-{
-#ifndef IAP_EMULATION
-    uint32_t* magicWord = BOOTLOADER_MAGIC_ADDRESS;
-    *magicWord = BOOTLOADER_MAGIC_WORD;
-    magicWord++;
-    *magicWord = physicalAddressToUse;
+static_assert(sizeof(BootloaderDescriptor) % FLASH_RAM_BUFFER_ALIGNMENT == 0,
+        "Check 4 byte alignment of BootloaderDescriptor, don't use packed!");
+
+
+/**
+ * Bootloader magic erase = FactoryResetWithoutIndividualAddress in calimero-core
+ */
+constexpr RestartPDUMasterReset BOOTLOADER_MAGIC_ERASE = T_MASTERRESET_FACTORY_WO_IA;
+
+/**
+ * Bootloader magic channel
+ */
+constexpr uint8_t BOOTLOADER_MAGIC_CHANNEL = 255;
+
+/**
+ * "Unique" BootloaderDescriptor identifier in RAM.
+ */
+constexpr uint32_t UID_BOOTLOADER_DESCRIPTOR = 0x5E1FB055;
+
+/**
+ * Pointer to BootloaderDescriptor in RAM.
+ * @warning The bootLoaderDescriptor is stored in RAM at 0x100000C0.
+ *          The application RAM must start behind the bootLoaderDescriptor to avoid overwriting it.
+ * @def bootLoaderDescriptor
+ */
+#ifdef IAP_EMULATION ///\todo must be #ifdef on release! fix before release!
+    uint32_t data;
+    uint32_t * magicWord = &data; // I'm here for the unit-tests
+    BootloaderDescriptor testBootLoaderDescriptor;
+    BootloaderDescriptor* bootLoaderDescriptor = &testBootLoaderDescriptor;
+#else
+    auto magicWord = reinterpret_cast<uint32_t*>(0x10000000UL);
+    auto bootLoaderDescriptor = reinterpret_cast<BootloaderDescriptor*>(0x100000C0UL);
 #endif
+
+void initBootloaderDescriptor(const BootState newBootState, const uint16_t physicalAddressToUse,
+    const uint32_t programmingButton, const uint32_t applicationId, const uint32_t applicationVersion)
+{
+    bootLoaderDescriptor->bootState = newBootState;
+    bootLoaderDescriptor->physicalAddress = physicalAddressToUse;
+    bootLoaderDescriptor->programmingButton = programmingButton;
+    bootLoaderDescriptor->applicationId = applicationId;
+    bootLoaderDescriptor->applicationVersion = applicationVersion;
+
+    switch (bootLoaderDescriptor->bootState)
+    {
+        case BootState::BootLoader:
+        case BootState::BootLoaderUpdater:
+        case BootState::Reset:
+            *magicWord = UID_BOOTLOADER_DESCRIPTOR;
+            break;
+
+        case BootState::Application:
+        default:
+            *magicWord = 0;
+            break;
+    }
 }
 
-bool checkApciForMagicWord(const byte eraseCode, const byte channelNumber)
+const BootloaderDescriptor* getBootloaderDescriptor()
+{
+    if (*magicWord != UID_BOOTLOADER_DESCRIPTOR)
+    {
+        return nullptr;
+    }
+    return bootLoaderDescriptor;
+}
+
+void clearBootloaderDescriptor()
+{
+    initBootloaderDescriptor(BootState::Application, 0, 0, 0, 0);
+}
+
+const BootloaderDescriptor* debugOnlyBootloaderDescriptor()
+{
+    return bootLoaderDescriptor;
+}
+
+bool checkApciForMagicWord(const uint8_t eraseCode, const uint8_t channelNumber)
 {
     // The special version of APCI_MASTER_RESET_PDU used by Selfbus bootloader
     // restart with parameters, special meaning of erase=7 and channel=255 for bootloader mode

firmware_updater/bootloader/src/bcu_updater.cpp
@@ -149,7 +149,8 @@ bool BcuUpdate::processBroadCastTelegram(const ApciCommand apciCmd, unsigned cha
             // Cache current physical address in RAM.
             // The next telegram will be an APCI_BASIC_RESTART_PDU
             // See KNX Spec. 3.0 3/5/2 2.3 NM_IndividualAddress_Write
-            prepareRestartIntoBootloader(this->ownAddress());
+            ///\todo set appId and appVersion
+            initBootloaderDescriptor(BootState::BootLoader, ownAddress(), static_cast<uint32_t>(progPin), 0, 0);
         }
     }
 

firmware_updater/bootloader/src/bootloader.cpp
@@ -41,23 +41,23 @@
 
 
 // bootloader specific settings
-#define RUN_MODE_BLINK_CONNECTED (250) //!< while connected, programming and run led blinking time in milliseconds
-#define RUN_MODE_BLINK_IDLE (1000)     //!< while idle/disconnected, programming and run led blinking time in milliseconds
-#define BL_RESERVED_RAM_START (0x10000000) //!< RAM start address for bootloader
+constexpr uint16_t RUN_MODE_BLINK_CONNECTED_MS = 250; //!< while connected, programming and run led blinking time in milliseconds
+constexpr uint16_t RUN_MODE_BLINK_IDLE_MS = 1000;     //!< while idle/disconnected, programming and run led blinking time in milliseconds
+constexpr uint32_t BL_RESERVED_RAM_START = 0x10000000UL;       //!< RAM start address for bootloader
 constexpr uint8_t BL_DEFAULT_VECTOR_TABLE_SIZE_IN_BYTES = 192; //!< vectortable size in bytes
 constexpr uint8_t BL_DEFAULT_VECTOR_TABLE_COUNT = BL_DEFAULT_VECTOR_TABLE_SIZE_IN_BYTES / sizeof(uintptr_t); //!< vector count to copy prior application start
 
-#define APP_START_DELAY_MS (250)          //!< Time in milliseconds the programming led will light before the app is started
+constexpr uint16_t APP_START_DELAY_MS = 250; //!< Time in milliseconds the programming led will light before the app is started
 
 // KNX/EIB specific settings
-#define DEFAULT_BL_KNX_ADDRESS (((15 << 12) | (15 << 8) | 192)) //!< 15.15.192 default updater KNX-address
+constexpr uint16_t DEFAULT_BL_KNX_ADDRESS = (((15 << 12) | (15 << 8) | 192)); //!< 15.15.192 default updater KNX-address
 
-BcuUpdate bcu = BcuUpdate(); //!< @ref BcuUpdate instance used for bus communication of the bootloader
+BcuUpdate bcu; //!< @ref BcuUpdate instance used for bus communication of the bootloader
 
 Timeout runModeTimeout; //!< running mode LED blinking timeout
 bool blinky = false;
 
-void startup();
+const BootloaderDescriptor* startup();
 
 uint32_t getProgrammingButton()
 {
@@ -71,8 +71,13 @@ uint32_t getProgrammingButton()
 }
 
 /**
- * @brief Starts BCU with @ref DEFAULT_BL_KNX_ADDRESS (15.15.192)
- *        Sets @ref PIN_RUN as output and in debug build also @ref PIN_INFO as output
+ * @brief Bootloader setup function.
+ *        Initializes the bcu and configures the programming button and physical address.
+ *        If a valid BootloaderDescriptor is found in RAM, its parameters are used.
+ *        Otherwise, default values are used.
+ * @note  In debug build also initializes the serial port for debugging output and
+ *        sets the @ref PIN_INFO and @ref PIN_RUN LEDs to output.
+ * @return Pointer to initialized BcuBase instance
  */
 BcuBase* setup()
 {
@@ -98,42 +103,60 @@ BcuBase* setup()
     }
 #endif
 
-    startup();
-    bcu.setOwnAddress(DEFAULT_BL_KNX_ADDRESS);
-    bcu.setProgPin(getProgrammingButton());
+    uint16_t physAddr;
+    uint32_t progButton;
+    const BootloaderDescriptor* blDescriptor = startup();
+    if (blDescriptor != nullptr)
+    {
+        physAddr = blDescriptor->physicalAddress;
+        progButton = blDescriptor->gpioProgrammingButton;
+        ///\todo After reset the application will be started (old behavior).
+        ///      We need to find the right place to clear the BootloaderDescriptor.
+        ///      Here it's too early. Best place would be, if we are 100% sure that the application is startable.
+        ///      But keep in mind, that a simple UPD_REQUEST_UID can also restart the bootloader.
+        clearBootloaderDescriptor();
+    }
+    else
+    {
+        physAddr = DEFAULT_BL_KNX_ADDRESS;
+        progButton = getProgrammingButton();
+    }
+    bcu.setOwnAddress(physAddr);
+    bcu.setProgPin(progButton);
     runModeTimeout.start(1);
 
     // finally start the bcu
     bcu.begin();
 
-#ifdef DEBUG
-    int physicalAddress = bcu.ownAddress();
-    serial.print("Selfbus KNX Bootloader v", BOOTLOADER_MAJOR_VERSION);
-    serial.print(".", BOOTLOADER_MINOR_VERSION, DEC);
-    serial.println(" DEBUG MODE :-)");
-    serial.print("Build                       : ");
-    serial.print(__DATE__);
-    serial.print(" ");
-    serial.println(__TIME__);
-    serial.print("Library                     : v", highByte(static_cast<uint16_t>(SBLIB_VERSION)), HEX); // lib version is in hexadecimal
-    serial.println(".", lowByte(SBLIB_VERSION), HEX);
-    serial.println("Features                    : 0x", BL_FEATURES, HEX);
-    serial.print("Flash      (start,end,size) : 0x", flashFirstAddress());
-    serial.print(" 0x", flashLastAddress());
-    serial.println(" 0x", flashSize(), HEX);
-    serial.print("Bootloader (start,end,size) : 0x", bootLoaderFirstAddress());
-    serial.print(" 0x", bootLoaderLastAddress());
-    serial.println(" 0x", bootLoaderSize(), HEX);
-    serial.println("Firmware (start)            : 0x", applicationFirstAddress());
-    serial.println("Boot descriptor (start)     : 0x", bootDescriptorBlockAddress());
-    serial.println("Boot descriptor page        : 0x", bootDescriptorBlockPage(), HEX);
-    serial.println("Boot descriptor size        : 0x", BOOT_BLOCK_DESC_SIZE, HEX);
-    serial.print("physical address            : ");
-    serial.print(physAddressToArea(physicalAddress));
-    serial.print(".", physAddressToLine(physicalAddress));
-    serial.println(".", physAddressToDevice(physicalAddress));
-    serial.println();
-#endif
+    dump(
+        serial.print("Selfbus KNX Bootloader v", BOOTLOADER_MAJOR_VERSION);
+        serial.print(".", BOOTLOADER_MINOR_VERSION, DEC);
+        serial.println(" DEBUG MODE :-)");
+        serial.print("Build                       : ");
+        serial.print(__DATE__);
+        serial.print(" ");
+        serial.println(__TIME__);
+        serial.print("Library                     : v", highByte(static_cast<uint16_t>(SBLIB_VERSION)), HEX); // lib version is in hexadecimal
+        serial.println(".", lowByte(SBLIB_VERSION), HEX);
+        serial.println("Features                    : 0x", BL_FEATURES, HEX);
+        serial.println("bootLoaderDescriptor addr.  : 0x", reinterpret_cast<uintptr_t>(debugOnlyBootloaderDescriptor()), HEX);
+        serial.print("Flash      (start,end,size) : 0x", flashFirstAddress());
+        serial.print(" 0x", flashLastAddress());
+        serial.println(" 0x", flashSize(), HEX);
+        serial.print("Bootloader (start,end,size) : 0x", bootLoaderFirstAddress());
+        serial.print(" 0x", bootLoaderLastAddress());
+        serial.println(" 0x", bootLoaderSize(), HEX);
+        serial.println("Firmware (start)            : 0x", applicationFirstAddress());
+        serial.println("Boot descriptor (start)     : 0x", bootDescriptorBlockAddress());
+        serial.println("Boot descriptor page        : 0x", bootDescriptorBlockPage(), HEX);
+        serial.println("Boot descriptor size        : 0x", BOOT_BLOCK_DESC_SIZE, HEX);
+        uint16_t physicalAddress = bcu.ownAddress();
+        serial.print("physical address            : ");
+        serial.print(physAddressToArea(physicalAddress));
+        serial.print(".", physAddressToLine(physicalAddress));
+        serial.println(".", physAddressToDevice(physicalAddress));
+        serial.println();
+    )
 
     return &bcu;
 }
@@ -147,14 +170,14 @@ void loop()
     {
         if (bcu.directConnection())
         {
-            runModeTimeout.start(RUN_MODE_BLINK_CONNECTED);
+            runModeTimeout.start(RUN_MODE_BLINK_CONNECTED_MS);
         }
         else
         {
 #if defined(DEBUG) && (!(defined(TS_ARM)))
             digitalWrite(PIN_INFO, false);  // Turn Off info LED
 #endif
-            runModeTimeout.start(RUN_MODE_BLINK_IDLE);
+            runModeTimeout.start(RUN_MODE_BLINK_IDLE_MS);
         }
         blinky = !blinky;
     }
@@ -177,7 +200,7 @@ void loop_noapp()
 /**
  * Restores MCU and register changes made by the bootloader (e.g. sysTick).
  */
-static void finalize()
+static inline void finalize()
 {
     Timeout ledTimeout; // don't use delay(), it needs nearly 100% more flash
     pinMode(getProgrammingButton(), OUTPUT);
@@ -191,32 +214,37 @@ static void finalize()
 }
 
 /**
- * @brief Starts the application by copying application's stack top and vectortable to ram, remaps it and calls Reset vector of it
- *
- * @param start     Start address of application
+ * Transfers control from bootloader to the application.
+ * @details This function performs the critical operation of starting the user application
+ *          by setting up the vector table and jumping to the application's reset handler.
+ *          The process involves:
+ *          1. Finalizing bootloader state (LEDs, timers)
+ *          2. Copying the application's vector table from flash to RAM
+ *          3. Remapping memory so RAM vectors are used instead of flash
+ *          4. Loading the application's stack pointer
+ *          5. Jumping to the application's reset vector
+ * 
+ * @param applicationStartAddress Pointer to the start address of the application in flash. 
+ * @note This function does not return. Control is transferred to the application.
  */
-static void jumpToApplication(uint8_t * start)
+[[noreturn]] static void jumpToApplication(uint8_t * applicationStartAddress)
 {
     finalize(); // restore changes made and turn the programming led on
-#ifndef IAP_EMULATION
-    unsigned int StackTop = *(unsigned int *) (start);
-    unsigned int ResetVector = *(unsigned int *) (start + 4);
-    unsigned int * rom = (unsigned int *) start;
-    unsigned int * ram = (unsigned int *) BL_RESERVED_RAM_START;
-    unsigned int i;
-    // copy the first 192 bytes (vector table) of the "application"
-    // into the RAM and then remap the vector table inside the RAM
+    const uint32_t* rom = reinterpret_cast<const uint32_t*>(applicationStartAddress);
+    uint32_t* ram = reinterpret_cast<uint32_t*>(BL_RESERVED_RAM_START);
+    uint32_t StackTop = rom[0];
+    uint32_t ResetVector = rom[1];
 
     dump(
         serial.print("Vectortable size: ", BL_DEFAULT_VECTOR_TABLE_SIZE_IN_BYTES);
-        serial.println(" bytes, Vector count: ", BL_DEFAULT_VECTOR_TABLE_COUNT);
+        serial.println(" bytes, #Vectors: ", BL_DEFAULT_VECTOR_TABLE_COUNT);
         serial.flush();
     );
 
-    for (i = 0; i < BL_DEFAULT_VECTOR_TABLE_COUNT; i++, rom++, ram++)
-    {
-        *ram = *rom;
-    }
+    // copy the first 192 bytes (vector table) of the application
+    // into the RAM and then remap the vector table inside the RAM
+    memcpy(ram, rom, BL_DEFAULT_VECTOR_TABLE_SIZE_IN_BYTES);
+
     LPC_SYSCON->SYSMEMREMAP = 0x01;
     // DO NOT use a __DSB here, even if the user manual UM10398 28.4.2.4 states it, otherwise application won't start.
 
@@ -227,39 +255,53 @@ static void jumpToApplication(uint8_t * start)
     asm volatile ("mov SP, %0" : : "r" (StackTop));
     /* Once the stack is setup we jump to the application reset vector */
     asm volatile ("bx      %0" : : "r" (ResetVector));
-#endif
+    __builtin_unreachable(); // Tell compiler this point is never reached
 }
 
 /**
- * @brief  Checks if "magic word" @ref BOOTLOADER_MAGIC_ADDRESS for bootloader mode is present and starts in bootloader mode.
- *         If no "magic word" is present it checks for a valid application to start,
- *         otherwise starts in bootloader mode
+ * Determines the startup mode of the bootloader.
+ * @details This function implements the startup logic with the following priority:
+ *          1. Checks if a BootloaderDescriptor was set by the application to request updater mode
+ *          2. Checks if the programming button is pressed at power-up to enter updater mode
+ *          3. Attempts to start the main application if it exists and is valid
+ *          4. Falls back to updater mode if no valid application is found
+ * 
+ * @return Pointer to BootloaderDescriptor if updater mode was requested by application, 
+ *         nullptr if updater mode should be entered (button pressed or no valid application),
+ *         or does not return if a valid application is started (jumps to application)
+ * 
+ * @note If a valid application is found, this function does not return and instead 
+ *       transfers control to the application via @ref jumpToApplication().
  */
-void startup()
+const BootloaderDescriptor* startup()
 {
-    // Updater request from application by setting magicWord
-    uint32_t* magicWord = BOOTLOADER_MAGIC_ADDRESS;
-    if (*magicWord == BOOTLOADER_MAGIC_WORD)
+    dump(serial.print("Bootloader startup -> ");)
+    // Updater request from application by setting BootloaderDescriptor
+    const BootloaderDescriptor* blDescriptor = getBootloaderDescriptor();
+    if (blDescriptor != nullptr)
     {
-        *magicWord = 0; // avoid restarting BL after flashing
-        return;
+        dump(serial.println("BootloaderDescriptor valid");)
+        return blDescriptor;
     }
-    *magicWord = 0; // wrong magicWord, delete it
 
     // Enter Updater when programming button was pressed at power up
     pinMode(getProgrammingButton(), INPUT | PULL_UP);
     if (!digitalRead(getProgrammingButton()))
     {
-        return;
+        dump(serial.println("Programming Button pressed");)
+        return nullptr;
     }
 
     // Start main application at address
-    AppDescriptionBlock * block = (AppDescriptionBlock *) bootDescriptorBlockAddress();
+    const AppDescriptionBlock* block = reinterpret_cast<const AppDescriptionBlock*>(bootDescriptorBlockAddress());
     if (checkApplication(block))
     {
+        dump(serial.println("Application valid");)
         jumpToApplication(block->startAddress);
     }
     // Start updater in case of error
+    dump(serial.println("Application INVALID");)
+    return nullptr;
 }
 
 /** @}*/

firmware_updater/bootloaderupdater/src/app_main.cpp
@@ -2,6 +2,7 @@
 #include <sblib/io_pin_names.h>
 #include <sblib/digital_pin.h>
 #include <sblib/version.h>
+#include <sblib/internal/bootloader_commands.h>
 #include <sblib/platform.h>
 #include <cstdint>
 #include <cstring>
@@ -46,7 +47,34 @@ void setup()
         serial.println(__TIME__);
         serial.flush();
     );
+
+    // Check for BootloaderDescriptor in RAM
+    const BootloaderDescriptor* blDescriptor = getBootloaderDescriptor();
+
+    dump(serial.print("BootloaderDescriptor ");)
+    if (blDescriptor != nullptr)
+    {
+        dump(serial.println("valid");)
+        gpioProgButton = blDescriptor->programmingButton;
+    }
+    else
+    {
+        dump(
+            serial.println("INVALID");
+            serial.flush();
+            const BootloaderDescriptor* debugOnlyDescriptor = debugOnlyBootloaderDescriptor();
+            serial.println("debugOnlyDescriptor 0x", &debugOnlyDescriptor);
+            if (debugOnlyDescriptor != nullptr)
+            {
+                serial.println("uid 0x", debugOnlyDescriptor->uidBootloaderDescriptor, HEX);
+                serial.println("physicalAddress 0x", debugOnlyDescriptor->physicalAddress, HEX);
+                serial.println("progButton 0x", debugOnlyDescriptor->programmingButton, HEX);
+                serial.println("appId 0x", debugOnlyDescriptor->applicationId, HEX);
+                serial.println("appVersion 0x", debugOnlyDescriptor->applicationVersion, HEX);
+            }
+        )
         gpioProgButton = PIN_PROG;
+    }
     pinMode(gpioProgButton, OUTPUT);
     digitalWrite(gpioProgButton, false);
 }
@@ -135,6 +163,7 @@ int main()
     else
     {
         dump(serial.println(" done");)
+        BootloaderDescriptor();
     }
 
     SystemReset();

sblib/src/eib/bcu_base.cpp
@@ -53,6 +53,9 @@ void BcuBase::_begin()
     TLayer4::_begin();
     bus->begin(ownAddress());
     progButtonDebouncer.init(1);
+#if defined(INCLUDE_SERIAL)
+    serial.println("bootLoaderDescriptor address: 0x", reinterpret_cast<uintptr_t>(debugOnlyBootloaderDescriptor()), HEX);
+#endif
 }
 
 void BcuBase::loop()
@@ -218,7 +221,7 @@ void BcuBase::softSystemReset()
     if (restartType == RestartType::MasterIntoBootloader)
     {
         noInterrupts();
-        prepareRestartIntoBootloader(this->ownAddress());
+        initBootloaderDescriptor(BootState::BootLoader , ownAddress(), progPin, 0, 0); ///\todo set appId and appVersion);
     }
 
     NVIC_SystemReset();

test/lib-test-cases/lib-test-cases.cmake
@@ -9,6 +9,7 @@ set(SBLIB_LIB_TEST_CASES_SRC
         src/prot_network_layer.cpp
         src/prot_parameter.cpp
         src/prot_physical_address.cpp
+        src/test_bootloader_commands.cpp
         src/test_datapoint_types.cpp
         src/test_digital_pin.cpp
         src/test_eeprom.cpp

commit 9e28d7bf2d5b63de2f97e8d78452c2a643192bff

Fix access violation if no RAM is reserved for `bootLoaderDescriptor`
This bug was introduced with commit 08ff753 and caused all apps that
are not for the bootloader (Flashstart) to effectively stop working.

sblib/inc/sblib/internal/bootloader_commands.h
@@ -23,6 +23,7 @@ enum class BootState : uint8_t
 
 /**
  * Bootloader descriptor structure stored in RAM to pass parameters to the bootloader.
+ * \note Total size is 20 bytes and must be reserved by the link script of the application
  */
 struct BootloaderDescriptor
 {
@@ -33,6 +34,8 @@ struct BootloaderDescriptor
     uint32_t applicationId;         //!< Application ID of the application
     uint32_t applicationVersion;    //!< Application version of the application
 };
+
+constexpr uint32_t BOOTLOADER_DESCRIPTOR_SIZE = 20;
 
 /**
  * Initialize the BootloaderDescriptor in RAM.

sblib/src/internal/bootloader_commands.cpp
@@ -33,7 +33,7 @@ constexpr uint32_t UID_BOOTLOADER_DESCRIPTOR = 0x5E1FB055;
  *          The application RAM must start behind the bootLoaderDescriptor to avoid overwriting it.
  * @def bootLoaderDescriptor
  */
-#ifdef IAP_EMULATION ///\todo must be #ifdef on release! fix before release!
+#ifdef IAP_EMULATION
     uint32_t data;
     uint32_t * magicWord = &data; // I'm here for the unit-tests
     BootloaderDescriptor testBootLoaderDescriptor;
@@ -41,11 +41,34 @@ constexpr uint32_t UID_BOOTLOADER_DESCRIPTOR = 0x5E1FB055;
 #else
     auto magicWord = reinterpret_cast<uint32_t*>(0x10000000UL);
     auto bootLoaderDescriptor = reinterpret_cast<BootloaderDescriptor*>(0x100000C0UL);
+    extern uint8_t __base_RAM; // NOLINT(*-reserved-identifier)
 #endif
+
+bool isRAMReserved()
+{
+#ifndef IAP_EMULATION
+    const auto ramBaseAddress = reinterpret_cast<uintptr_t>(&__base_RAM);
+    if (ramBaseAddress >= (reinterpret_cast<uintptr_t>(bootLoaderDescriptor) + BOOTLOADER_DESCRIPTOR_SIZE))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+#else
+    return true;
+#endif
+}
 
 void initBootloaderDescriptor(const BootState newBootState, const uint16_t physicalAddressToUse,
     const uint32_t programmingButton, const uint32_t applicationId, const uint32_t applicationVersion)
 {
+    if (!isRAMReserved())
+    {
+        return;
+    }
+
     bootLoaderDescriptor->bootState = newBootState;
     bootLoaderDescriptor->physicalAddress = physicalAddressToUse;
     bootLoaderDescriptor->programmingButton = programmingButton;
@@ -69,6 +92,11 @@ void initBootloaderDescriptor(const BootState newBootState, const uint16_t physi
 
 const BootloaderDescriptor* getBootloaderDescriptor()
 {
+    if (!isRAMReserved())
+    {
+        return nullptr;
+    }
+
     if (*magicWord != UID_BOOTLOADER_DESCRIPTOR)
     {
         return nullptr;