0b93323e9757ac311803cef6caaff8883dd4a627
Darthyson <darth@maptrack.de>
2026-06-24 12:31:01 +0200

    Add MurmurHash3 docu

--- sblib/inc/sblib/murmur_hash_3.h
+++ sblib/inc/sblib/murmur_hash_3.h
@@ -3,6 +3,7 @@
 // domain. The author hereby disclaims copyright to this source code.
 
 // origin https://github.com/aappleby/smhasher
+// online MurmurHash3 calculator https://www.codertools.net/tools/murmurhash.php
 
 #ifndef SBLIB_MURMURHASH3_H_
 #define SBLIB_MURMURHASH3_H_
@@ -10,6 +11,14 @@
 #include <cstdint>
 
 
+/**
+ * @brief Computes the MurmurHash3 x86 32-bit hash of the given key.
+ * 
+ * @param key Pointer to the data to be hashed
+ * @param len Length of the data in bytes
+ * @param seed Seed value for the hash function (default 0)
+ * @param out Pointer to the output buffer (must be at least 4 bytes)
+ */
 void murmurHash3_x86_32(const void * key, uint32_t len, uint32_t seed, void * out);
 
 #endif /* SBLIB_MURMURHASH3_H_ */
